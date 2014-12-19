31de766e5f5399823b37a2a4f2a7e239f3425689
张睿 <zrui16@hotmail.com>
2014-12-19 19:30:19 +0800

    将BitIO中的byte域设为unsigned char类型。
    之前被错误设为了char类型。

--- bitio.h
+++ bitio.h
@@ -2,13 +2,13 @@
 
 typedef struct {
 	FILE *f;
-	char byte;
+	unsigned char byte;
 	size_t bitcnt;
 } BitIO;
 
 extern void BitIO_init      (BitIO *, FILE *);
 extern int  BitIO_read      (BitIO *);
 extern int  BitIO_read_byte (BitIO *);
-extern int  BitIO_write     (BitIO *, int);
-extern int  BitIO_write_byte(BitIO *, int);
-extern int  BitIO_flush     (BitIO *);
+extern void  BitIO_write     (BitIO *, int);
+extern void  BitIO_write_byte(BitIO *, int);
+extern void  BitIO_flush     (BitIO *);
